512dc2eb26913033e03082f7859c5281db0d5d20
DomenFlakusBosilj <domen.4fb@gmail.com>
2019-01-09 23:16:04 +0100

    read.csv -> read_csv pri vseh dodan locale urejen Germany(...) -> Germany
    pravilen uvoz

--- uvoz/uvoz_domen.r
+++ uvoz/uvoz_domen.r
@@ -7,20 +7,24 @@ loc <- locale(encoding = "UTF-8", decimal_mark = ".", grouping_mark = ",")
 
 #Tabela 1 
 
-delez_ljudi <- read.csv('podatki/ljudje_delezi.csv', encoding = 'windows-1250',
-                    col.names = c('Leto', 'Drzava', 'Prevozno_sredstvo', 'krnekaj',
-                                  'Delez', 'krnekaj'), na = c(':', ''))
+delez_ljudi <- read_csv('podatki/ljudje_delezi.csv', locale = loc, skip = 1,
+                    col_names = c('Leto', 'Drzava', 'Prevozno_sredstvo', 'krnekaj',
+                                  'Delez', 'krnekaj1'), na = c(':', ''))
 delez_ljudi <- delez_ljudi[,-6]
 delez_ljudi <- delez_ljudi[,-4]
+
+delez_ljudi$Drzava <- gsub("^Germany.*", "Germany", delez_ljudi$Drzava)
 
 
 #Tabela 2
 
-delez_tovora <- read.csv('podatki/tovor_delezi.csv', encoding = 'windows-1250',
-                        col.names = c('Leto', 'Drzava', 'Prevozno_sredstvo', 'krnekaj',
-                                      'Delez', 'krnekaj'), na = c(':', ''))
+delez_tovora <- read_csv('podatki/tovor_delezi.csv', locale = loc, skip = 1,
+                        col_names = c('Leto', 'Drzava', 'Prevozno_sredstvo', 'krnekaj',
+                                      'Delez', 'krnekaj1'), na = c(':', ''))
 delez_tovora <- delez_tovora[,-6]
 delez_tovora <- delez_tovora[,-4]
+
+delez_tovora$Drzava <- gsub("^Germany.*", "Germany", delez_tovora$Drzava)
 
 
 #Tabela 3
@@ -30,8 +34,6 @@ smrti_avti_bus <- read_csv('podatki/smrti_avti_busi.csv', locale = loc, skip=1,
                                          'Stevilo', 'krnekaj2'), na = c(':', '', ''))
 smrti_avti_bus <- smrti_avti_bus[,-6]
 smrti_avti_bus <- smrti_avti_bus[,-3]
-
-#smrti_avti_bus <- smrti_avti_bus %>% spread(Prevozno_sredstvo, Stevilo)
 
 
 #Tabela 4
@@ -44,20 +46,23 @@ smrti_vlak <- smrti_vlak[,-6]
 smrti_vlak <- smrti_vlak[,-5]
 smrti_vlak <- smrti_vlak[,-4]
 smrti_vlak <- smrti_vlak[,-3]
+
 smrti_vlak$Prevozno_sredstvo <- "Vlak"
 
 
 #Tabela 5
 
-greenhouse_gas <- read.csv('podatki/greenhouse_gas.csv', encoding = 'windows-1250',
-                       col.names = c('Leto', 'Drzava', 'airpol', 'nace', 'unit',
-                                     'Kolicina.kg', 'krnekaj'), na = c(':', ''))
+greenhouse_gas <- read_csv('podatki/greenhouse_gas.csv', locale = loc, skip=1,
+                       col_names = c('Leto', 'Drzava', 'airpol', 'nace', 'unit',
+                                     'Kolicina_kg', 'krnekaj'), na = c(':', ''))
 greenhouse_gas <- greenhouse_gas[,-7]
 greenhouse_gas <- greenhouse_gas[,-5]
 greenhouse_gas <- greenhouse_gas[,-4]
 greenhouse_gas <- greenhouse_gas[,-3]
 
 greenhouse_gas <- greenhouse_gas %>% filter(Drzava != "European Union (current composition)")
+
+greenhouse_gas$Drzava <- gsub("^Germany.*", "Germany", greenhouse_gas$Drzava)
 
 
 #Tabela 6
@@ -71,21 +76,21 @@ bdppc <- bdppc[,-3]
 
 bdppc <- bdppc %>% filter(Drzava != "European Union (current composition)")
 
+bdppc$Drzava <- gsub("^Germany.*", "Germany", bdppc$Drzava)
+
 
 #Tabela 7
-
-#smrti <- left_join(smrti_avti_bus, smrti_vlak)
-#names(smrti) <- c("Leto", "Drzava", 'Avtobus', 'Avto', 'Vlak')
-
-#smrti <- melt(smrti, variable.name = "Prevozno_sredstvo", value.name = 'Stevilo', measure.vars=3:5)
-
-#smrti[,4] <- as.integer(smrti[,4])
-#smrti[,2] <- as.character(smrti[,2])
 
 smrti <- rbind(smrti_avti_bus, smrti_vlak) %>%
   filter(Drzava != "European Union (current composition)")
 
 smrti$Drzava <- gsub("^Germany.*", "Germany", smrti$Drzava)
+
+smrti$Prevozno_sredstvo <- gsub("Passenger cars", "Avto", smrti$Prevozno_sredstvo)
+smrti$Prevozno_sredstvo <- gsub("Buses", "Bus", smrti$Prevozno_sredstvo)
+
+# grp <- group_by(smrti, Prevozno_sredstvo)
+# smrti_sum <- summarise(grp, sum(Stevilo))
 
 
 #Shranjevanje tidy tabel
