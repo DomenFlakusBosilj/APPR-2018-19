f197ea1262b4d9c1d3b251eff37a7993586fc1ea
DomenFlakusBosilj <domen.4fb@gmail.com>
2019-01-16 00:00:02 +0100

    graf delez ljudi spremenjen v geom_line iz geom_bar

--- vizualizacija/vizualizacija_domen.R
+++ vizualizacija/vizualizacija_domen.R
@@ -15,23 +15,29 @@ graf_bdppc <- ggplot(data = bdppc, mapping = aes(x=Drzava, y=Kolicina_eur, group
   theme(axis.text.x = element_text(angle = 90, size = 8)) +
   theme(legend.text = element_text(size=8))
 
-# ggplot(data = bdppc, mapping = aes(x=Leto, y=Kolicina_eur, color=Drzava)) +
-#   geom_line()
-
 
 #graf greenhouse gas
-graf_greenhouse <- ggplot(data = greenhouse_gas, mapping = aes(x=Leto, y=Kolicina.kg, color=Drzava)) +
-  geom_line(stat = 'identity', position = 'dodge')
+graf_gas_pmio <- ggplot(data = gas_pmio, mapping = aes(x=Drzava, y=Kolicina_kg_na_mio, fill=Leto)) +
+  geom_bar(stat = 'identity', position = 'dodge') +
+  theme(axis.text.x = element_text(angle = 90, size = 8))
 
 
+#graf delez ljudi
+# graf_delez_ljudi <- ggplot(data = delez_ljudi, mapping = aes(x=Drzava, y=Delez,
+#                                                             fill=Prevozno_sredstvo, group=Leto)) +
+#   geom_bar(stat = 'identity',position = 'dodge') +
+#   theme(axis.text.x = element_text(angle = 90, size = 8))
 
+graf_delez_ljudi <- ggplot(data = delez_ljudi, mapping = aes(x=Leto, y=Delez,
+                                                             color=Prevozno_sredstvo)) +
+  ggtitle("Delez uporabe prevoznih sredstev") +
+  geom_line() +
+  facet_grid(. ~ Drzava) +
+  theme(axis.text.x = element_text(angle = 90, size = 5)) 
+# +
+#   theme(legend.title = element_text('Prevozno sredstvo'))
 
-graf_delez_ljudi <- ggplot(data = delez_ljudi, mapping = aes(x=Leto, y=Delez, 
-                                                            fill=Prevozno_sredstvo, group=Drzava)) +
-  geom_bar(stat = 'identity', position = 'dodge')
-
-
-
+#graf smrti
 graf_smrti <- ggplot(data = smrti, mapping = aes(x=Drzava, y=Stevilo,
                                                  fill=Prevozno_sredstvo, group=Leto)) +
   geom_bar(stat = 'identity', position = 'dodge') +
@@ -50,9 +56,7 @@ svet <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearth
 europe <- filter(svet, CONTINENT == "Europe")
 europe <- filter(europe, long < 55 & long > -45 & lat > 30 & lat < 85)
 
-ggplot(europe, aes(x=long, y=lat, group=group, fill=NAME)) +
+graf_europe <- ggplot(europe, aes(x=long, y=lat, group=group, fill=NAME)) +
   geom_polygon() +
   labs(title="Evropa - brez podatkov") +
   theme(legend.position="none")
-
-
