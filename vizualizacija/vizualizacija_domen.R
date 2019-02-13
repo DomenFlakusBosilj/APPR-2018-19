842b396fd53e9b4035cef8566dd9b903199c4824
DomenFlakusBosilj <domen.4fb@gmail.com>
2019-02-13 19:56:19 +0100

    urejene osi grafov

--- vizualizacija/vizualizacija_domen.R
+++ vizualizacija/vizualizacija_domen.R
@@ -17,25 +17,21 @@ graf_bdppc <- ggplot(data = bdppc, mapping = aes(x=Drzava, y=Kolicina_eur, group
 
 
 #graf greenhouse gas
-graf_gas_pmio <- ggplot(data = gas_pmio, mapping = aes(x=Drzava, y=Kolicina_kg_na_mio, fill=factor(Leto))) +
+graf_gas_pmio <- ggplot(data = gas_pmio, mapping = aes(x=Drzava, y=Kolicina_kg_na_mio,
+                                                       fill=factor(Leto))) +
+  ggtitle('Količina toplogrednih plinov v ozracju') +
   geom_bar(stat = 'identity', position = 'dodge') +
+  labs(x = 'Drzava', y = 'kg na milijon prebivalcev') +
   theme(axis.text.x = element_text(angle = 90, size = 8))
 
 
 #graf delez ljudi
-
 graf_delez_ljudi <- ggplot(data = delez_ljudi, mapping = aes(x=Leto, y=Delez,
                                                              color=Prevozno_sredstvo)) +
-  ggtitle("Delez uporabe prevoznih sredstev") +
+  ggtitle("Delezi uporabe prevoznih sredstev") +
   geom_line() +
-  facet_grid( Drzava~.) +
+  facet_grid(Drzava~.) +
   theme(axis.text.x = element_text(angle = 90, size = 5)) 
-
-# +
-#   theme(legend.title = element_text('Prevozno sredstvo'))# graf_delez_ljudi <- ggplot(data = delez_ljudi, mapping = aes(x=Drzava, y=Delez,
-#                                                             fill=Prevozno_sredstvo, group=Leto)) +
-#   geom_bar(stat = 'identity',position = 'dodge') +
-#   theme(axis.text.x = element_text(angle = 90, size = 8))
 
 
 #graf smrti
@@ -56,7 +52,7 @@ svet <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearth
 
 # Zemljevid sveta skrčimo na zemljevid Evrope
 europe <- filter(svet, CONTINENT == "Europe")
-europe <- filter(europe, long < 55 & long > -45 & lat > 30 & lat < 85)
+europe <- filter(europe, long < 55 & long > -35 & lat > 30 & lat < 85)
 
 europe <- filter(europe, NAME != "Jersey")
 europe <- filter(europe, NAME != "Russia")
@@ -71,10 +67,12 @@ names(drzave) <- "Drzava"
 skupaj <- left_join(drzave, smrti_sum_pmio_2016, by="Drzava")
 skupaj$Stevilo[is.na(skupaj$Stevilo)]<- 0
 
-# Izrišem zemljevid Evrope, v katerem bo vsaka država pobarvana glede št. igralcev v ligi NBA
+# Zemljevid s podatki
 zemljevid_smrti_sum_pmio_2016 <- ggplot() + geom_polygon(data=left_join(europe, skupaj, by=c("NAME"="Drzava")),
                        aes(x=long, y=lat, group=group, fill=Stevilo)) +
   geom_line() +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
   guides(fill=guide_colorbar(title="Število smrti")) +
   ggtitle("Število smrti v prometu na milijon prebivalcev leta 2016") +
   labs(x = " ") +
@@ -82,47 +80,3 @@ zemljevid_smrti_sum_pmio_2016 <- ggplot() + geom_polygon(data=left_join(europe, 
   scale_fill_gradient(low = "white", high = "red",
                       space = "Lab", na.value = "grey", guide = "black",
                       aesthetics = "fill")
-
-
-# europe <- filter(europe, SOVEREIGNT != "Albania")
-# europe <- filter(europe, SOVEREIGNT != "Andorra")
-# europe <- filter(europe, SOVEREIGNT != "Montenegro")
-# europe <- filter(europe, SOVEREIGNT != "Moldova")
-# europe <- filter(europe, SOVEREIGNT != "Macedonia")
-# europe <- filter(europe, SOVEREIGNT != "Monaco")
-# europe <- filter(europe, SOVEREIGNT != "Malta")
-# europe <- filter(europe, SOVEREIGNT != "Liechtenstein")
-# europe <- filter(europe, SOVEREIGNT != "Kosovo")
-# europe <- filter(europe, SOVEREIGNT != "Liechtenstein")
-# europe <- filter(europe, SOVEREIGNT != "Iceland")
-# europe <- filter(europe, SOVEREIGNT != "Belarus")
-# europe <- filter(europe, SOVEREIGNT != "Bosnia and Herz.")
-# europe <- filter(europe, SOVEREIGNT != "Faeroe Is.")
-# europe <- filter(europe, SOVEREIGNT != "Guernsey")
-# europe <- filter(europe, SOVEREIGNT != "^Isle.*")
-# europe <- filter(europe, SOVEREIGNT != "Jersey")
-# europe <- filter(europe, SOVEREIGNT != "Russia")
-# europe <- filter(europe, SOVEREIGNT != "Serbia")
-# europe <- filter(europe, SOVEREIGNT != "San Marino")
-# europe <- filter(europe, SOVEREIGNT != "Ukraine")
-# europe <- filter(europe, SOVEREIGNT != "Vatican")
-# 
-# europe <- filter(europe, NAME != "Guernsey")
-# europe <- filter(europe, NAME != "Jersey")
-# europe <- filter(europe, NAME != "Ã…land")
-# europe <- filter(europe, NAME != "^Bosnia.*")
-# 
-# graf_europe <- ggplot(europe, aes(x=long, y=lat, group=group, fill=NAME)) +
-#   geom_polygon() +
-#   labs(title="Evropa - brez podatkov") +
-#   theme_classic()
-
-
-
-# zemljevid_smrti <- ggplot() +
-#   geom_polygon(data = data = povprecje %>% right_join(europe, by = c("regija" = "NAME_1")),
-#                aes(x = long, y = lat, group = Prevozno_sredstvo, fill = Stevilo))+
-#   xlab("") + ylab("") + ggtitle("Število porok po slovenskih regijah")
-# 
-# zemljevid_smrti + scale_fill_gradient(low = "#132B43", high = "#56B1F7", space = "Lab",
-#                                        na.value = "grey50", guide = "colourbar")
