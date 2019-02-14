d9b28fd909536ac91dcd162301a65e47beeb426f
DomenFlakusBosilj <domen.4fb@gmail.com>
2019-02-14 18:40:10 +0100

    graf_smrti mali popravki zemljevid 1 mali popravki

--- vizualizacija/vizualizacija_domen.R
+++ vizualizacija/vizualizacija_domen.R
@@ -19,24 +19,38 @@ graf_bdppc <- ggplot(data = bdppc, mapping = aes(x=Drzava, y=Kolicina_eur, group
 #graf greenhouse gas
 graf_gas_pmio <- ggplot(data = gas_pmio, mapping = aes(x=Drzava, y=Kolicina_kg_na_mio,
                                                        fill=factor(Leto))) +
-  ggtitle('Količina toplogrednih plinov v ozracju') +
+  labs(fill='Leto') +
+  ggtitle('Količina toplogrednih plinov v ozračju') +
   geom_bar(stat = 'identity', position = 'dodge') +
-  labs(x = 'Drzava', y = 'kg na milijon prebivalcev') +
+  labs(x = 'Država', y = 'kg na milijon prebivalcev') +
   theme(axis.text.x = element_text(angle = 90, size = 8))
 
 
 #graf delez ljudi
-graf_delez_ljudi <- ggplot(data = delez_ljudi, mapping = aes(x=Leto, y=Delez,
-                                                             color=Prevozno_sredstvo)) +
-  ggtitle("Delezi uporabe prevoznih sredstev") +
+graf_delez_ljudi <- ggplot(data = filter(delez_ljudi, Drzava == 'Austria' | Drzava == 'Bulgaria' |
+                                         Drzava == 'Croatia' | Drzava == 'Czechia' | Drzava == 'Estonia' |
+                                         Drzava == 'France' | Drzava == 'Hungary' | Drzava == 'Ireland' |
+                                         Drzava == 'Lithuania' | Drzava == 'Sweden' | Drzava == 'Poland' |
+                                         Drzava == 'Slovenia' | Drzava == 'Switzerland'),
+                           mapping = aes(x=Leto, y=Delez, color=Prevozno_sredstvo)) +
+  labs(color='Prevozno sredstvo') +
+  ggtitle("Deleži uporabe prevoznih sredstev") +
+  labs(x = 'Leto', y = 'Delež') +
   geom_line() +
   facet_grid(Drzava~.) +
   theme(axis.text.x = element_text(angle = 90, size = 5)) 
 
 
 #graf smrti
-graf_smrti <- ggplot(data = smrti_pmio, mapping = aes(x=Leto, y=Stevilo,
-                                                 color=Prevozno_sredstvo, group=Leto)) +
+graf_smrti <- ggplot(data = filter(smrti_pmio, Drzava == 'Austria' | Drzava == 'Bulgaria' |
+                                     Drzava == 'Croatia' | Drzava == 'Czechia' | Drzava == 'Estonia' |
+                                     Drzava == 'France' | Drzava == 'Hungary' | Drzava == 'Ireland' |
+                                     Drzava == 'Lithuania' | Drzava == 'Sweden' | Drzava == 'Poland' |
+                                     Drzava == 'Slovenia' | Drzava == 'Switzerland'),
+                     mapping = aes(x=Leto, y=Stevilo, color=Prevozno_sredstvo, group=Leto)) +
+  labs(color='Prevozno sredstvo') +
+  ggtitle('Smrti v prometu z različnimi prevoznimi sredstvi') +
+  labs(x = 'Leto', y = 'Število') +
   geom_point(stat = 'identity', position = 'dodge') +
   facet_grid( Drzava~.) +
   theme(axis.text.x = element_text(angle = 90, size = 6))
@@ -64,19 +78,19 @@ drzave <- unique(europe$NAME)
 drzave <- as.data.frame(drzave, stringsAsFactors=FALSE) 
 names(drzave) <- "Drzava"
 
-skupaj <- left_join(drzave, smrti_sum_pmio_2016, by="Drzava")
-skupaj$Stevilo[is.na(skupaj$Stevilo)]<- 0
+skupaj <- left_join(drzave, smrti_sum_pmio_2015, by="Drzava")
+#skupaj$Stevilo[is.na(skupaj$Stevilo)]<- 0
 
 # Zemljevid s podatki
-zemljevid_smrti_sum_pmio_2016 <- ggplot() + geom_polygon(data=left_join(europe, skupaj, by=c("NAME"="Drzava")),
+zemljevid_smrti_sum_pmio_2015 <- ggplot() + geom_polygon(data=left_join(europe, skupaj, by=c("NAME"="Drzava")),
                        aes(x=long, y=lat, group=group, fill=Stevilo)) +
   geom_line() +
   theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.text.y=element_blank(),
         axis.ticks.y=element_blank()) +
   guides(fill=guide_colorbar(title="Število smrti")) +
-  ggtitle("Število smrti v prometu na milijon prebivalcev leta 2016") +
+  ggtitle("Število smrti v prometu na milijon prebivalcev leta 2015") +
   labs(x = " ") +
   labs(y = " ") +
   scale_fill_gradient(low = "white", high = "red",
-                      space = "Lab", na.value = "grey", guide = "black",
+                      space = "Lab", na.value = "#e0e0d1", guide = "black",
                       aesthetics = "fill")
